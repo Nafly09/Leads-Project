826f8209495a6ec436d9210fd594f577b1a3af0e
Naftaly Junior <juniorinterlyte95@hotmail.com>
2022-03-06 20:17:18 -0300

    added database files

--- app/configs/database.py
+++ app/configs/database.py
@@ -1,11 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+from dotenv import load_dotenv
 import os
 
 db = SQLAlchemy()
 
 
 def init_app(app: Flask):
+    load_dotenv()
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DB_URI")
 
